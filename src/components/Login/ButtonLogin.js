d8c9a8ad4394278741484c3a504765c22705a951
Josue <92354998+JosueLopezO@users.noreply.github.com>
2022-08-29 02:35:27 +0000

    Agrega la vista Login
    Se crean los componentes de la vista Login y el componente LoginPage

---
+++ src/components/Login/ButtonLogin.js
@@ -0,0 +1,7 @@
+export default function ButtonLogin() {
+    return (  
+    <div className="flex justify-end">
+        <input type="button" value="Iniciar" className="btn btn-primary" />
+    </div>
+    );
+}
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,15 @@
+import ButtonLogin from '../components/Login/ButtonLogin.js'
+import CardLogin from '../components/Login/CardLogin.js'
+import InfoLogin from '../components/Login/InfoLogin.js'
+import InputLogin from '../components/Login/InputLogin.js'
+
+export default function LoginPage() {
+    return (
+        <CardLogin>
+            <p className="text-2xl text-center">Iniciar Sesión</p>
+            <InputLogin name="Matrícula" placeHolderText="2202020201" />
+            <InputLogin name="Fecha de nacimiento" placeHolderText="30122000" info={<InfoLogin text="La contraseña es tu fecha de nacimiento con el formato: DDMMAAAA" />} />
+            <ButtonLogin />
+        </CardLogin>
+    );
+}
