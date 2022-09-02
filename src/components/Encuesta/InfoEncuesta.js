e0c48f7c7549c76ea4c6a1de95a0bc9bcdcdffe0
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-09-02 12:32:52 -0500

    Agrega componente InfoEncuesta

---
+++ src/components/Encuesta/InfoEncuesta.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function InfoEncuesta(){
+  return(
+    <div className="px-4 pb-2">
+      <p className="text-xs font-normal text-slate-500">
+        Las opciones por defecto son en modalidad <b>Presencial</b> y en 
+        horario <b>Sin preferencia</b>.
+        <br/>
+
+        Para cambiar estas opciones puedes dar al botón a la derecha de cada
+        materia (<b>Opciones</b> en computadora y <b>+</b> en dispositivos
+        móviles).
+      </p>
+    </div>
+  )
+}
+
+export default InfoEncuesta
--- src/pages/EncuestaPage.js
+++ src/pages/EncuestaPage.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 
 import ButtonEnviarEncuesta from '../components/Encuesta/ButtonEnviarEncuesta';
-import InfoEncuesta from '../components/Encuesta/InfoEncuesta';
+import HeaderEncuesta from '../components/Encuesta/HeaderEncuesta';
+import InfoEncuesta from "../components/Encuesta/InfoEncuesta";
 import TablaMateriasEncuesta from "../components/Encuesta/TablaMateriasEncuesta";
 
 // Context de las materias en elegidas por el usuario
@@ -85,9 +86,11 @@ function EncuestaPage() {
 
     {/* <MateriasEncuestaContext.Provider value={{materiasEncuesta, setMateriasEncuesta}}> */}
 
-      <InfoEncuesta user={user} maxMaterias={maxMaterias} />
+      <HeaderEncuesta user={user} maxMaterias={maxMaterias} />
 
-      <ButtonEnviarEncuesta/>
+      <ButtonEnviarEncuesta />
+
+      <InfoEncuesta />
 
       {/* Tabla */}
       <TablaMateriasEncuesta materias={materias} 
@@ -99,7 +102,7 @@ function EncuestaPage() {
         <TablaMateriasEncuesta materias={materias} maxMaterias={maxMaterias} />
       </MateriasEncuestaContext.Consumer> */}
 
-      <ButtonEnviarEncuesta/>
+      <ButtonEnviarEncuesta />
 
     {/* </MateriasEncuestaContext.Provider> */}
 
