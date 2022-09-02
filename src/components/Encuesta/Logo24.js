6c3f4cf3522480bca7d703a8dfeb4ba292f4da33
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-09-02 00:39:28 -0500

    Agrega componente Logo24

---
+++ src/components/Encuesta/Logo24.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import logoUAM from "../../assets/logo-uam.png";
+
+function Logo24() {
+  return (
+  <div className='avatar w-24'>
+    <img src={logoUAM} alt="Logo de encuestas uami" />
+  </div>);
+}
+    
+export default Logo24
