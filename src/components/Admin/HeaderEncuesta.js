3e7a64ae508190e7a3b8afec5d4eb3a475d83798
MOK-Zero <lgdesentis@gmail.com>
2022-09-04 15:03:39 -0500

    add Header nuevo

--- src/components/Admin/HeaderEncuesta.js
+++ src/components/Admin/HeaderEncuesta.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import Logo24 from "./Logo24";
 
+// Head Board
 function HeaderEncuesta({user, maxMaterias}) {
   return (
     <>
+        {/* Title Welcome */}
         <div className="navbar bg-base-100 space-x-4">
-            
             <div className="flex-none">
               <Logo24/>
             </div>
@@ -18,23 +19,24 @@ function HeaderEncuesta({user, maxMaterias}) {
         </div>
 
 
-        <div className="navbar bg-base-100 space-x-4">
-        
-        
-        <label class="input-group">
-                <span>NUM. UEAS ENCUESTA</span>
-                <input type="number" placeholder="Número" class="input input-bordered" />
-        </label>
-
-        <button class="btn">
-                Activar encuesta
-            </button><button class="btn">
-                UEAs abiertas
-            </button><button class="btn">
-                Estadisticas
-            </button>
-            
+      {/*Board options*/}
+      <div class="navbar bg-base-100">
+        <div class="flex-1">
+          {/*Limiter Option*/}
+          <label class="input-group">
+                  <span>NUM. UEAS ENCUESTA</span>
+                  <input type="number" placeholder="Número" class="input input-bordered" />
+          </label>
         </div>
+        <div class="flex-none">
+          {/*Group Buttons*/}
+          <ul class="menu menu-horizontal p-0">
+            <li><a>Activar Encuesta</a></li>
+            <li><a>UEAS Abiertas</a></li>
+            <li><a>Estadísticas</a></li>
+          </ul>
+        </div>
+      </div>
 
     </>
   );
