cac1bb2ce52c093e121408189922973ba08de5d9
Josue <92354998+JosueLopezO@users.noreply.github.com>
2022-09-11 01:43:55 +0000

    Agrega placeholder de las licenciaturas

---
+++ src/data/LICENCIATURAS.js
@@ -0,0 +1,111 @@
+const LICENCIATURAS =[
+  {
+    "clave": 1,
+    "nombre": "Licenciatura en Administración"
+  },
+  {
+    "clave": 2,
+    "nombre": "Licenciatura en Antropología Social"
+  },
+  {
+    "clave": 3,
+    "nombre": "Licenciatura en Biología"
+  },
+  {
+    "clave": 4,
+    "nombre": "Licenciatura en Biología Experimental"
+  },
+  {
+    "clave": 5,
+    "nombre": "Licenciatura en Ciencias Atmosféricas"
+  },
+  {
+    "clave": 6,
+    "nombre": "Licenciatura en Ciencia Política"
+  },
+  {
+    "clave": 7,
+    "nombre": "Licenciatura en Computación"
+  },
+  {
+    "clave": 8,
+    "nombre": "Licenciatura en Economía"
+  },
+  {
+    "clave": 9,
+    "nombre": "Licenciatura en Filosofía"
+  },
+  {
+    "clave": 10,
+    "nombre": "Licenciatura en Física"
+  },
+  {
+    "clave": 11,
+    "nombre": "Licenciatura en Geografía Humana"
+  },
+  {
+    "clave": 12,
+    "nombre": "Licenciatura en Hidrobiología"
+  },
+  {
+    "clave": 13,
+    "nombre": "Licenciatura en Historia"
+  },
+  {
+    "clave": 14,
+    "nombre": "Licenciatura en Ingeniería Biomédica"
+  },
+  {
+    "clave": 15,
+    "nombre": "Licenciatura en Ingeniería Electrónica"
+  },
+  {
+    "clave": 16,
+    "nombre": "Licenciatura en Ingeniería en Energía"
+  },
+  {
+    "clave": 17,
+    "nombre": "Licenciatura en Ingeniería Hidrológica"
+  },
+  {
+    "clave": 18,
+    "nombre": "Licenciatura en Ingeniería Química"
+  },
+  {
+    "clave": 19,
+    "nombre": "Licenciatura en Letras Hispánicas"
+  },
+  {
+    "clave": 20,
+    "nombre": "Licenciatura en Lingüística"
+  },
+  {
+    "clave": 21,
+    "nombre": "Licenciatura en Matemáticas"
+  },
+  {
+    "clave": 22,
+    "nombre": "Licenciatura en Producción Animal"
+  },
+  {
+    "clave": 23,
+    "nombre": "Licenciatura en Psicología Social"
+  },
+  {
+    "clave": 24,
+    "nombre": "Licenciatura en Química"
+  },
+  {
+    "clave": 25,
+    "nombre": "Licenciatura en Sociología"
+  },
+  {
+    "clave": 26,
+    "nombre": "Licenciatura en Ingeniería Bioquímica Industrial"
+  },
+  {
+    "clave": 27,
+    "nombre": "Licenciatura en Ingeniería de los Alimentos"
+  }
+]
+export default LICENCIATURAS
