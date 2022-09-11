d5ea2cd6e72f4d1cc456bb6d180305f020b4d9b9
Josue <92354998+JosueLopezO@users.noreply.github.com>
2022-09-11 01:43:21 +0000

    Agrega placeholder de las UEAS de computacion

---
+++ src/data/UEAS-COMPUTACION.js
@@ -0,0 +1,724 @@
+const UEAS = [
+  {
+    "clave": 2000001,
+    "nombre": "ARTE Y CIENCIA"
+  },
+  {
+    "clave": 2000003,
+    "nombre": "ECOLOGIA Y ECONOMIA"
+  },
+  {
+    "clave": 2000004,
+    "nombre": "HISTORIA Y SITUACION ACTUAL DEL AGUA EN EL VALLE DE MEXICO"
+  },
+  {
+    "clave": 2100001,
+    "nombre": "METODO EXPERIMENTAL I"
+  },
+  {
+    "clave": 2100002,
+    "nombre": "LABORATORIO DE SIMULACION"
+  },
+  {
+    "clave": 2100003,
+    "nombre": "METODO EXPERIMENTAL II"
+  },
+  {
+    "clave": 2100005,
+    "nombre": "CURSOS COMPLEMENTARIOS"
+  },
+  {
+    "clave": 2110018,
+    "nombre": "ELECTRICIDAD Y MAGNETISMO ELEMENTAL I"
+  },
+  {
+    "clave": 2110019,
+    "nombre": "MECANICA ELEMENTAL I"
+  },
+  {
+    "clave": 2110020,
+    "nombre": "MECANICA ELEMENTAL II"
+  },
+  {
+    "clave": 2110021,
+    "nombre": "FLUIDOS Y CALOR"
+  },
+  {
+    "clave": 2111042,
+    "nombre": "LABORATORIO DE OPTICA"
+  },
+  {
+    "clave": 2111115,
+    "nombre": "FISICA EXPERIMENTAL INTERMEDIA I"
+  },
+  {
+    "clave": 2111153,
+    "nombre": "FISICA COMPUTACIONAL"
+  },
+  {
+    "clave": 2122059,
+    "nombre": "MECANICA DE FLUIDOS"
+  },
+  {
+    "clave": 2122063,
+    "nombre": "TRANSFERENCIA DE CALOR"
+  },
+  {
+    "clave": 2122086,
+    "nombre": "FUNDAMENTOS Y MODELOS DE OPTIMIZACION"
+  },
+  {
+    "clave": 2122087,
+    "nombre": "METODOS NUMERICOS APLICADOS A LA INGENIERIA"
+  },
+  {
+    "clave": 2124062,
+    "nombre": "METODOS MATEMATICOS EN LA INGENIERIA DE PROCESOS"
+  },
+  {
+    "clave": 2130030,
+    "nombre": "INTRODUCCION AL PENSAMIENTO MATEMATICO"
+  },
+  {
+    "clave": 2130035,
+    "nombre": "ALGEBRA LINEAL APLICADA I"
+  },
+  {
+    "clave": 2130038,
+    "nombre": "CALCULO DIFERENCIAL"
+  },
+  {
+    "clave": 2130039,
+    "nombre": "CALCULO INTEGRAL"
+  },
+  {
+    "clave": 2130040,
+    "nombre": "CALCULO DE VARIAS VARIABLES I"
+  },
+  {
+    "clave": 2131041,
+    "nombre": "ESTADISTICA Y DISEÑO DE EXPERIMENTOS"
+  },
+  {
+    "clave": 2131091,
+    "nombre": "ECUACIONES DIFERENCIALES ORDINARIAS I"
+  },
+  {
+    "clave": 2131092,
+    "nombre": "ECUACIONES DIFERENCIALES PARCIALES"
+  },
+  {
+    "clave": 2131093,
+    "nombre": "METODOS NUMERICOS"
+  },
+  {
+    "clave": 2131094,
+    "nombre": "PROBABILIDAD APLICADA"
+  },
+  {
+    "clave": 2131100,
+    "nombre": "MATEMATICAS DISCRETAS I"
+  },
+  {
+    "clave": 2131101,
+    "nombre": "MATEMATICAS DISCRETAS II"
+  },
+  {
+    "clave": 2131108,
+    "nombre": "TEORIA DE GRUPOS"
+  },
+  {
+    "clave": 2131131,
+    "nombre": "CRIPTOGRAFIA DE CLAVE PUBLICA"
+  },
+  {
+    "clave": 2131161,
+    "nombre": "TEORIA DE GRAFICAS"
+  },
+  {
+    "clave": 2132015,
+    "nombre": "TEORIA DE JUEGOS"
+  },
+  {
+    "clave": 2132028,
+    "nombre": "ANALISIS COMBINATORIO"
+  },
+  {
+    "clave": 2132037,
+    "nombre": "TEMAS SELECTOS DE LA MATEMATICA CONTEMPORANEA I"
+  },
+  {
+    "clave": 2132040,
+    "nombre": "TEMAS SELECTOS DE MATEMATICAS APLICADAS I"
+  },
+  {
+    "clave": 2132069,
+    "nombre": "CALCULO DE VARIAS VARIABLES II"
+  },
+  {
+    "clave": 2132074,
+    "nombre": "ALGEBRA LINEAL APLICADA II"
+  },
+  {
+    "clave": 2140008,
+    "nombre": "TRANSFORMACIONES QUIMICAS"
+  },
+  {
+    "clave": 2140009,
+    "nombre": "ESTRUCTURA DE LA MATERIA"
+  },
+  {
+    "clave": 2150004,
+    "nombre": "INTRODUCCION A LA INGENIERIA ELECTRONICA"
+  },
+  {
+    "clave": 2150005,
+    "nombre": "INTRODUCCION A LA COMPUTACION"
+  },
+  {
+    "clave": 2151014,
+    "nombre": "REDES DE TELECOMUNICACIONES"
+  },
+  {
+    "clave": 2151035,
+    "nombre": "METODOS COMPUTACIONALES EN INGENIERIA BIOMEDICA"
+  },
+  {
+    "clave": 2151040,
+    "nombre": "INTERFACES PROGRAMABLES"
+  },
+  {
+    "clave": 2151052,
+    "nombre": "VISUALIZACION POR COMPUTADORAS DE IMAGENES MEDICAS"
+  },
+  {
+    "clave": 2151066,
+    "nombre": "CIRCUITOS ELECTRICOS I"
+  },
+  {
+    "clave": 2151074,
+    "nombre": "FUNDAMENTOS DE LOGICA DIGITAL"
+  },
+  {
+    "clave": 2151080,
+    "nombre": "SISTEMAS CON MICROPROCESADORES I"
+  },
+  {
+    "clave": 2151103,
+    "nombre": "FUNDAMENTOS DE PROGRAMACION"
+  },
+  {
+    "clave": 2151104,
+    "nombre": "ALGORITMOS Y PATRONES DE ALMACENAMIENTO LINEALES ORIENTADOS A OBJETOS"
+  },
+  {
+    "clave": 2151105,
+    "nombre": "ALGORITMOS Y PATRONES DE ALMACENAMIENTO NO LINEALES ORIENTADOS A OBJETOS"
+  },
+  {
+    "clave": 2151106,
+    "nombre": "BASES DE DATOS"
+  },
+  {
+    "clave": 2151107,
+    "nombre": "TEORIA MATEMATICA DE LA COMPUTACION"
+  },
+  {
+    "clave": 2151108,
+    "nombre": "ANALISIS Y DISEÑO DE SISTEMAS DE COMPUTACION"
+  },
+  {
+    "clave": 2151109,
+    "nombre": "COMPUTACION Y SU ENTORNO EMPRESARIAL"
+  },
+  {
+    "clave": 2151110,
+    "nombre": "COMPILADORES"
+  },
+  {
+    "clave": 2151111,
+    "nombre": "PROGRAMACION CONCURRENTE"
+  },
+  {
+    "clave": 2151112,
+    "nombre": "INGENIERIA DE SOFTWARE"
+  },
+  {
+    "clave": 2151113,
+    "nombre": "INTELIGENCIA ARTIFICIAL"
+  },
+  {
+    "clave": 2151114,
+    "nombre": "SISTEMAS OPERATIVOS"
+  },
+  {
+    "clave": 2151115,
+    "nombre": "ARQUITECTURA DE COMPUTADORAS"
+  },
+  {
+    "clave": 2151116,
+    "nombre": "ANALISIS Y DISEÑO DE ALGORITMOS"
+  },
+  {
+    "clave": 2151118,
+    "nombre": "APRENDIZAJE MAQUINAL"
+  },
+  {
+    "clave": 2151119,
+    "nombre": "LENGUAJES DE PROGRAMACION"
+  },
+  {
+    "clave": 2151121,
+    "nombre": "TECNICAS HEURISTICAS BIO-INSPIRADAS EN LA OPTIMIZACION"
+  },
+  {
+    "clave": 2151122,
+    "nombre": "TEMAS SELECTOS DE INTELIGENCIA ARTIFICIAL"
+  },
+  {
+    "clave": 2151123,
+    "nombre": "TEMAS SELECTOS DE BASES DE DATOS"
+  },
+  {
+    "clave": 2151124,
+    "nombre": "TEMAS SELECTOS DE INGENIERIA DE SOFTWARE"
+  },
+  {
+    "clave": 2151125,
+    "nombre": "TEMAS SELECTOS DE CIENCIAS DE LA COMPUTACION"
+  },
+  {
+    "clave": 2151130,
+    "nombre": "PROYECTO DE INVESTIGACION I BASES DE DATOS"
+  },
+  {
+    "clave": 2151131,
+    "nombre": "PROYECTO DE INVESTIGACION II BASES DE DATOS"
+  },
+  {
+    "clave": 2151132,
+    "nombre": "PROYECTO DE INVESTIGACION I CIENCIAS DE LA COMPUTACION"
+  },
+  {
+    "clave": 2151133,
+    "nombre": "PROYECTO DE INVESTIGACION II CIENCIAS DE LA COMPUTACION"
+  },
+  {
+    "clave": 2151134,
+    "nombre": "PROYECTO DE INVESTIGACION I INGENIERIA DE SOFTWARE"
+  },
+  {
+    "clave": 2151135,
+    "nombre": "PROYECTO DE INVESTIGACION II INGENIERIA DE SOFTWARE"
+  },
+  {
+    "clave": 2151136,
+    "nombre": "PROYECTO DE INVESTIGACION I INTELIGENCIA ARTIFICIAL"
+  },
+  {
+    "clave": 2151137,
+    "nombre": "PROYECTO DE INVESTIGACION II INTELIGENCIA ARTIFICIAL"
+  },
+  {
+    "clave": 2211075,
+    "nombre": "ADMINISTRACION GENERAL"
+  },
+  {
+    "clave": 2211076,
+    "nombre": "HISTORIA GENERAL DE LA EMPRESA Y SOCIEDAD ORGANIZACIONAL"
+  },
+  {
+    "clave": 2211078,
+    "nombre": "TEORIA ADMINISTRATIVA"
+  },
+  {
+    "clave": 2211079,
+    "nombre": "ORGANIZACION DEL TRABAJO Y COMPORTAMIENTO HUMANO"
+  },
+  {
+    "clave": 2211081,
+    "nombre": "TEORIA DE LA ORGANIZACION"
+  },
+  {
+    "clave": 2211082,
+    "nombre": "SOCIEDAD DE CONSUMO, DESARROLLO TECNOLOGICO Y COMUNICACION"
+  },
+  {
+    "clave": 2211083,
+    "nombre": "INFORMACION FINANCIERA"
+  },
+  {
+    "clave": 2211085,
+    "nombre": "COMPORTAMIENTO EN LAS ORGANIZACIONES"
+  },
+  {
+    "clave": 2211087,
+    "nombre": "CONTABILIDAD ADMINISTRATIVA"
+  },
+  {
+    "clave": 2211089,
+    "nombre": "DIRECCION Y ADMINISTRACION DE PERSONAL"
+  },
+  {
+    "clave": 2211090,
+    "nombre": "ETICA Y RESPONSABILIDAD SOCIAL"
+  },
+  {
+    "clave": 2211091,
+    "nombre": "FUNDAMENTOS DE POLITICA ECONOMICA"
+  },
+  {
+    "clave": 2211092,
+    "nombre": "ANALISIS DE DECISIONES"
+  },
+  {
+    "clave": 2211093,
+    "nombre": "MARKETING I"
+  },
+  {
+    "clave": 2211094,
+    "nombre": "GESTION Y CONTROL ESTRATEGICOS I"
+  },
+  {
+    "clave": 2211095,
+    "nombre": "ADMINISTRACION FINANCIERA I"
+  },
+  {
+    "clave": 2211096,
+    "nombre": "MARKETING II"
+  },
+  {
+    "clave": 2211097,
+    "nombre": "GESTION Y CONTROL ESTRATEGICOS II"
+  },
+  {
+    "clave": 2211098,
+    "nombre": "ADMINISTRACION DE OPERACIONES"
+  },
+  {
+    "clave": 2211099,
+    "nombre": "ADMINISTRACION FINANCIERA II"
+  },
+  {
+    "clave": 2212000,
+    "nombre": "PROBLEMAS FILOSOFICOS Y CIVILIZACION OCCIDENTAL"
+  },
+  {
+    "clave": 2212001,
+    "nombre": "DERECHO I"
+  },
+  {
+    "clave": 2212002,
+    "nombre": "DERECHO II"
+  },
+  {
+    "clave": 2212006,
+    "nombre": "VALUACION DE PROYECTOS"
+  },
+  {
+    "clave": 2212008,
+    "nombre": "METODOLOGIA DE ANALISIS DE PROBLEMAS EN LAS ORGANIZACIONES"
+  },
+  {
+    "clave": 2212009,
+    "nombre": "HERRAMIENTAS CUALITATIVAS PARA EL ANALISIS DE DECISIONES"
+  },
+  {
+    "clave": 2212010,
+    "nombre": "MODELOS DE DECISION EN LA GESTION DIRECTIVA"
+  },
+  {
+    "clave": 2212011,
+    "nombre": "INFORMATICA APLICADA PARA LA TOMA DE DECISIONES"
+  },
+  {
+    "clave": 2212012,
+    "nombre": "PROCESOS DE TOMA DE DECISIONES. ANALISIS DE CASOS"
+  },
+  {
+    "clave": 2212013,
+    "nombre": "LIDERAZGO"
+  },
+  {
+    "clave": 2212014,
+    "nombre": "HABILIDADES DIRECTIVAS"
+  },
+  {
+    "clave": 2212015,
+    "nombre": "ADMINISTRACION DEL CAMBIO ESTRATEGICO"
+  },
+  {
+    "clave": 2212016,
+    "nombre": "GESTION DEL CONOCIMIENTO"
+  },
+  {
+    "clave": 2212017,
+    "nombre": "GESTION DE PROYECTOS"
+  },
+  {
+    "clave": 2212018,
+    "nombre": "SISTEMAS DE GESTION DE CALIDAD"
+  },
+  {
+    "clave": 2212019,
+    "nombre": "AUDITORIA ADMINISTRATIVA"
+  },
+  {
+    "clave": 2212036,
+    "nombre": "E-MARKETING"
+  },
+  {
+    "clave": 2212038,
+    "nombre": "SISTEMA DE INFORMACION INTERNACIONAL"
+  },
+  {
+    "clave": 2212039,
+    "nombre": "TENDENCIAS MUNDIALES DEL MARKETING"
+  },
+  {
+    "clave": 2224041,
+    "nombre": "CULTURA"
+  },
+  {
+    "clave": 2224042,
+    "nombre": "MESOAMERICA"
+  },
+  {
+    "clave": 2224060,
+    "nombre": "ANTROPOLOGIA SIMBOLICA"
+  },
+  {
+    "clave": 2241088,
+    "nombre": "INTRODUCCION A LA MICROECONOMIA I"
+  },
+  {
+    "clave": 2241096,
+    "nombre": "MACROECONOMIA I"
+  },
+  {
+    "clave": 2250035,
+    "nombre": "TEORIA Y PROBLEMAS SOCIOPOLITICOS CONTEMPORANEOS"
+  },
+  {
+    "clave": 2250036,
+    "nombre": "HISTORIA CONTEMPORANEA DE MEXICO"
+  },
+  {
+    "clave": 2250037,
+    "nombre": "ARGUMENTACION Y CONOCIMIENTO"
+  },
+  {
+    "clave": 2255064,
+    "nombre": "INGLES INTERMEDIO I"
+  },
+  {
+    "clave": 2255065,
+    "nombre": "INGLES INTERMEDIO II"
+  },
+  {
+    "clave": 2255066,
+    "nombre": "INGLES INTERMEDIO III"
+  },
+  {
+    "clave": 2255100,
+    "nombre": "INTRODUCCION A LA HISTORIA"
+  },
+  {
+    "clave": 2255183,
+    "nombre": "PRACTICAS DE LECTURA Y REDACCION"
+  },
+  {
+    "clave": 2255187,
+    "nombre": "LITERATURA CLASICA"
+  },
+  {
+    "clave": 2255195,
+    "nombre": "LITERATURA HISPANOAMERICANA. SIGLOS XVI-XVIII"
+  },
+  {
+    "clave": 2255302,
+    "nombre": "INTRODUCCION A LA FILOSOFIA"
+  },
+  {
+    "clave": 2255303,
+    "nombre": "ETICA I"
+  },
+  {
+    "clave": 2255305,
+    "nombre": "ETICA II"
+  },
+  {
+    "clave": 2255317,
+    "nombre": "FILOSOFIA DE LA CIENCIA"
+  },
+  {
+    "clave": 2255363,
+    "nombre": "LENGUAJE Y CULTURA"
+  },
+  {
+    "clave": 2291099,
+    "nombre": "VARIABLES MACROECONOMICAS DE MEXICO"
+  },
+  {
+    "clave": 2293011,
+    "nombre": "INTRODUCCION A LA SOCIOLOGIA"
+  },
+  {
+    "clave": 2293034,
+    "nombre": "FUNDAMENTOS DE GEOGRAFIA HISTORICA"
+  },
+  {
+    "clave": 2293037,
+    "nombre": "GEOGRAFIA DE MEXICO"
+  },
+  {
+    "clave": 2293039,
+    "nombre": "GEOGRAFIA DEL TURISMO"
+  },
+  {
+    "clave": 2293042,
+    "nombre": "TEORIAS DEL DESARROLLO LOCAL"
+  },
+  {
+    "clave": 2293044,
+    "nombre": "PENSAMIENTO AMBIENTALISTA"
+  },
+  {
+    "clave": 2293046,
+    "nombre": "MAPA AMBIENTAL DE MEXICO"
+  },
+  {
+    "clave": 2293054,
+    "nombre": "SOCIOLOGIA URBANA"
+  },
+  {
+    "clave": 2293055,
+    "nombre": "ORDENAMIENTO TERRITORIAL"
+  },
+  {
+    "clave": 2293056,
+    "nombre": "TEMAS SELECTOS DE GEOGRAFIA III"
+  },
+  {
+    "clave": 2293057,
+    "nombre": "TEMAS SELECTOS DE GEOGRAFIA IV"
+  },
+  {
+    "clave": 2293058,
+    "nombre": "ANTROPOLOGIA URBANA"
+  },
+  {
+    "clave": 2293089,
+    "nombre": "SOCIOLOGIA DEL TRABAJO"
+  },
+  {
+    "clave": 2293096,
+    "nombre": "SOCIOLOGIA DE LOS MOVIMIENTOS SOCIALES"
+  },
+  {
+    "clave": 2293097,
+    "nombre": "METODOS Y TECNICAS DE ACCION SOCIAL"
+  },
+  {
+    "clave": 2294001,
+    "nombre": "DIDACTICA DE LA EDUCACION EN MATERIA SOCIAL"
+  },
+  {
+    "clave": 2294002,
+    "nombre": "SOCIOLOGIA DE LA POBLACION"
+  },
+  {
+    "clave": 2294003,
+    "nombre": "SOCIOLOGIA RURAL"
+  },
+  {
+    "clave": 2294004,
+    "nombre": "SOCIOLOGIA POLITICA"
+  },
+  {
+    "clave": 2294006,
+    "nombre": "SOCIOLOGIA DE LA GLOBALIZACION"
+  },
+  {
+    "clave": 2294008,
+    "nombre": "SOCIOLOGIA AMBIENTAL"
+  },
+  {
+    "clave": 2294009,
+    "nombre": "SOCIOLOGIA DE GENERO"
+  },
+  {
+    "clave": 2312057,
+    "nombre": "CLIMATOLOGIA"
+  },
+  {
+    "clave": 2312061,
+    "nombre": "EVOLUCION"
+  },
+  {
+    "clave": 2312063,
+    "nombre": "ECOLOGIA I"
+  },
+  {
+    "clave": 2312064,
+    "nombre": "ECOLOGIA II"
+  },
+  {
+    "clave": 2312065,
+    "nombre": "BIOGEOGRAFIA"
+  },
+  {
+    "clave": 2312083,
+    "nombre": "COMPORTAMIENTO ANIMAL"
+  },
+  {
+    "clave": 2312087,
+    "nombre": "BIOCONSERVACION"
+  },
+  {
+    "clave": 2312089,
+    "nombre": "ECOLOGIA EVOLUTIVA"
+  },
+  {
+    "clave": 2312090,
+    "nombre": "ECOLOGIA URBANA"
+  },
+  {
+    "clave": 2321059,
+    "nombre": "INTRODUCCION A LOS SISTEMAS DE INFORMACION GEOGRAFICA"
+  },
+  {
+    "clave": 2321064,
+    "nombre": "TALLER DE BIOMETRIA AGROPECUARIA I"
+  },
+  {
+    "clave": 2321066,
+    "nombre": "TALLER DE BIOMETRIA AGROPECUARIA II"
+  },
+  {
+    "clave": 2331078,
+    "nombre": "GESTION Y CONTROL DE CALIDAD"
+  },
+  {
+    "clave": 2331082,
+    "nombre": "INGENIERIA DE ALIMENTOS I"
+  },
+  {
+    "clave": 2331083,
+    "nombre": "INGENIERIA DE ALIMENTOS II"
+  },
+  {
+    "clave": 2331084,
+    "nombre": "INGENIERIA DE ALIMENTOS III"
+  },
+  {
+    "clave": 2331093,
+    "nombre": "BIOQUIMICA DE LA NUTRICION"
+  },
+  {
+    "clave": 2352038,
+    "nombre": "GENETICA"
+  }
+]
+
+export default UEAS
