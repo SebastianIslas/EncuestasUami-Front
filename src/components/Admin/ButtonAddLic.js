bae9d92bb736cd40de3e42c6d0f525d92680e06d
Josue <92354998+JosueLopezO@users.noreply.github.com>
2022-09-11 01:46:03 +0000

    Agrega funcionalidad a los botones de la vista home del admin
    Se agrega funcionalidad de Agregar, Editar y Eliminar licencitura en el panel de admin

--- src/components/Admin/ButtonAddLic.js
+++ src/components/Admin/ButtonAddLic.js
@@ -1,9 +1,10 @@
-export default function ButtonAddLic() {
+export default function ButtonAddLic({onClick}) {
   return (  
     <div className='flex flex-row-reverse py-4'>
       <button type="submit"
           className='btn btn-primary 
-                     btn-xs sm:btn-sm md:btn-md'>
+                     btn-xs sm:btn-sm md:btn-md'
+              onClick={onClick}>
       Agregar Licenciatura
       </button>
     </div>
--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import ButtonAddLic from '../components/Admin/ButtonAddLic.js';
 import AdminHomeHeader from '../components/Admin/AdminHomeHeader';
 import TablaLicsAdmin from "../components/Admin/TablaLicsAdmin";
+import ModalAgregar from "../components/Admin/ModalAgregar.js";
 
+import LICENCIATURAS from "../data/LICENCIATURAS.js";
 
 function AdminPage() {
 
@@ -13,18 +15,19 @@ function AdminPage() {
   // Lista la licenciatura solo usamos la clave y el nombre
   const [licenciaturas, setLicenciaturas] = useState([]);
 
+  const [showModalAgregar, setShowModalAgregar] = useState(false);
+
 
   useEffect(() => {
     document.title = "Panel de Administracion";
-
-    setLicenciaturas([
-      {clave: 11111111, nombre: "Licenciatura en Balsamiq 1"},
-      {clave: 22222222, nombre: "Licenciatura en Balsamiq 2"},
-      {clave: 33333333, nombre: "Licenciatura en Balsamiq 3"},
-      {clave: 44444444, nombre: "Licenciatura en Balsamiq 4"}
-    ]);
-
+    setLicenciaturas(LICENCIATURAS);
   }, []);
+
+
+  
+  const toggleModalAgregar = () => {
+    setShowModalAgregar(!showModalAgregar);
+  }
 
   return (
   <div className="bg-base-200">
@@ -37,8 +40,14 @@ function AdminPage() {
                       setLicenciaturas={setLicenciaturas}
       />
 
-      <ButtonAddLic />
+      <ButtonAddLic onClick={toggleModalAgregar} />
 
+      {showModalAgregar ? <ModalAgregar
+          showModal={showModalAgregar}
+          setShowModal={setShowModalAgregar}
+          licenciaturas={licenciaturas}
+          setLicenciaturas={setLicenciaturas}
+           /> : null}
 
   </div>
   </div>);
