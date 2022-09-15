9d32ca66725b6adcfade804ab21e493520b2d3a5
MOK-Zero <lgdesentis@gmail.com>
2022-09-14 22:09:02 -0500

    fix warnings html

--- src/components/Home/NavBarDaisy.js
+++ src/components/Home/NavBarDaisy.js
@@ -14,8 +14,8 @@ export default function NavBarDaisy() {
     
         <div className="w-full min-h-max navbar bg-base-300">
             <div className="flex-none lg:hidden">
-                <label for="my-drawer-3" className="btn btn-square btn-ghost">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                 </label>
             </div> 
             <div className="flex-1 px-2 mx-2 text-5xl"><strong>Encuestas UAMI</strong></div>
@@ -35,11 +35,11 @@ export default function NavBarDaisy() {
 
     </div> 
     <div className="drawer-side">
-        <label for="my-drawer-3" className="drawer-overlay"></label> 
+        <label htmlFor="my-drawer-3" className="drawer-overlay"></label> 
         <ul className="menu p-4 overflow-y-auto w-80 bg-base-100">
             <li>
-                <label for="my-drawer-3" className="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                 </label>
             </li>
             <li><Link to="/login">Login</Link></li>
