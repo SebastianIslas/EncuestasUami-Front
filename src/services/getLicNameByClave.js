68711465acaa97ccc45a5bef06f5db1830a40cc5
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-09-15 13:52:10 -0500

    BugFix AdminUeas
    - Arregla error con la clave de la Licenciatura obtenida es un string,
      lo cual causa problemas con los servicios.

--- src/services/getLicNameByClave.js
+++ src/services/getLicNameByClave.js
@@ -3,10 +3,12 @@ import LICENCIATURAS from "../data/LICENCIATURAS"
 // Función que regresa el nombre de una Licenciatura por su clave
 export function getLicNameByClave(claveLic) {
   for (let i = 0; i < LICENCIATURAS.length; i++){
-    if (LICENCIATURAS[i].clave == claveLic){
-      return [LICENCIATURAS[i].nombre];
+    // NOTE: Luis: lo pongo a to String y no parseInt porque no estoy seguro que
+    // las claves de las materias son sólo numéros
+    if (claveLic === LICENCIATURAS[i].clave.toString()){
+      return LICENCIATURAS[i].nombre;
     }
   }
 
-  return "ERROR: No se encontró licenciatura con la clave" + claveLic;
+  return "ERROR: No se encontró licenciatura con la clave " + claveLic;
 }
